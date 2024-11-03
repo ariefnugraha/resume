d0b9a7f7a3c997aac46b034921b0152b8144bba6
arief <muhammadariefnugraha77@gmail.com>
2024-11-04 00:03:10 +0700

    ubah layout, buat pakai tailwind, ubah boxicon jadi font awesome, ubah gambar, ubah konten

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    colors: {
+      'primary-surface': "#CFD6E0",
+      'primary-border': "#B0BBCB",
+      'primary-main': "#113263",
+      "primary-hover": "#0B2142",
+      "black": "#121212",
+      "white": "#fff",
+      "gray": "#818181",
+    },
+    fontFamily: {
+      'sans': ['Satoshi', 'sans-serif'],
+      'clash-display': ['Clash Display', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
